bb6a23d7767ac35f063fcb64b6a65483c6366718
patelsonam <patelsonam926@gmail.com>
2026-01-15 20:47:48 +0530

    Day 3: Added core models and backend verification

--- eduvillage/core/admin.py
+++ eduvillage/core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Village, UserProfile, Course
 
-# Register your models here.
+admin.site.register(Village)
+admin.site.register(UserProfile)
+admin.site.register(Course)
--- eduvillage/core/models.py
+++ eduvillage/core/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Village(models.Model):
+    name = models.CharField(max_length=100)
+    district = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class UserProfile(models.Model):
+    ROLE_CHOICES = [
+        ('student', 'Student'),
+        ('teacher', 'Teacher'),
+        ('admin', 'Admin'),
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+    village = models.ForeignKey(Village, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
