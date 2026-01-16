36a0522c728b427e86154da84c266b9a0522a72f
patelsonam <patelsonam926@gmail.com>
2026-01-16 18:39:53 +0530

    Day 4: Added templates, navigation links, and frontend pages

--- eduvillage/core/urls.py
+++ eduvillage/core/urls.py
@@ -4,3 +4,10 @@ from .views import home
 urlpatterns = [
     path('', home, name='home'),
 ]
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+]
--- eduvillage/core/views.py
+++ eduvillage/core/views.py
@@ -2,3 +2,13 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Welcome to EduVillage – Learning for Every Village")
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'core/home.html')
+
+def about(request):
+    return render(request, 'core/about.html')
+# Home page view
+def home(request):
+    return render(request, 'core/home.html')
